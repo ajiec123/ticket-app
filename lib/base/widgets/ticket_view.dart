4def80d7a3bb6e30af3589253ac3106299be3944
ajiec123 <ajirahmat17@gmail.com>
2025-01-06 22:24:21 +0700

    add ticket view

---
+++ lib/base/widgets/ticket_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TicketView extends StatelessWidget {
+  const TicketView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return SizedBox(
+      width: size.width * 0.85,
+    );
+  }
+}
